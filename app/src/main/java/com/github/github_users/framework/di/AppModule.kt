ce240852dba10eaef35bb086b245d40ede46c872
Mahsa Alamshahi <alamshahi.mahsa@gmail.com>
2022-09-16 15:57:20 +0430

    DI added.

---
+++ app/src/main/java/com/github/github_users/framework/di/AppModule.kt
@@ -0,0 +1,29 @@
+package com.github.github_users.framework.di
+
+import android.content.Context
+import com.github.github_users.presentation.NavigationHelper
+import com.github.github_users.presentation.UserListAdapter
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+
+
+    @Provides
+    fun provideAppContext(@ApplicationContext context: Context): Context = context
+
+    @Provides
+    fun provideNavigationHelper() = NavigationHelper()
+
+    @Provides
+    fun provideListAdapter(navigationHelper: NavigationHelper) = UserListAdapter(navigationHelper)
+
+
+}
