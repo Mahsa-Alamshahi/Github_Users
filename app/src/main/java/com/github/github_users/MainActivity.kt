a34a648fb7f97a8eb0c5c264ac24a563da7e7da8
Mahsa Alamshahi <alamshahi.mahsa@gmail.com>
2022-09-16 15:51:47 +0430

    Navigation added.

--- app/src/main/java/com/github/github_users/MainActivity.kt
+++ app/src/main/java/com/github/github_users/MainActivity.kt
@@ -2,7 +2,9 @@ package com.github.github_users
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/github/github_users/presentation/GithubUsersApplication.kt
@@ -0,0 +1,9 @@
+package com.github.github_users.presentation
+
+import androidx.multidex.MultiDexApplication
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class GithubUsersApplication: MultiDexApplication() {
+}
